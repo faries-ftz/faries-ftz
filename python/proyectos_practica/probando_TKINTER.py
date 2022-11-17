5465b5c63c6d11ff1ca2a987009bbf96cecfaea4
FARIES <ftobala@gmail.com>
2022-11-17 14:24:16 -0500

    actualizacion de grafica

--- python/proyectos_practica/probando_TKINTER.py
+++ python/proyectos_practica/probando_TKINTER.py
@@ -1,14 +1,14 @@
 import random
 import tkinter
 #tkinter es una interfaz grafica que se dice viene instalada en windows, para linux se necesita intalarla 
-
-ven=tkinter.Tk()#qui asignas -ven- como ventana grafica
-ven.geometry("300x300") #por default tiene un tamaño pequeño, aqui especificamos sus dimenciones
-ven.mainloop()
+def grafica():
+    ven=tkinter.Tk()#qui asignas -ven- como ventana grafica
+    ven.geometry("300x300") #por default tiene un tamaño pequeño, aqui especificamos sus dimenciones
+    ven.mainloop()
 
 x=int(input("put a number: "))
 def guess(x):
     ran_num=random.randint(1,x)
     print(f"su numero es {x}")
 guess(x)
-
+grafica()
