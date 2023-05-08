bdab2a3c23dc5148dcb86ee39878ccecbe73e988
FARIES <ftobala@gmail.com>
2023-05-08 11:21:10 -0500

    rs

---
+++ python/Aprendizaje/W3School/variables.py
@@ -0,0 +1,39 @@
+"""
+esto es un comentario entre comillas
+pondre en practica muhcos de mis conocimientos
+para el desarrollo en 
+python
+"""
+#   las variables pueden tomar cualquier tipo de dato
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#   to ask for the data type you could use type()
+print(type(x))
+print(type(y))
+# variiables have somre rules 
+""""
+-- A variable name must start with a letter or the underscore character
+-- A variable name cannot start with a number
+-- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+-- Variable names are case-sensitive (age, Age and AGE are three different variables)
+"""
+myvar = "John"
+my_var = "John"
+_my_var = "John"                       
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+# cabe resaltar que las variables respetan las practicas de usar camle case, pascal,case o snake case
+
+#   camel case: Each word, except the first, starts with a capital letter
+myVariableName = "John"
+#   pascal case: Each word starts with a capital letter:
+MyVariableName = "John"
+#   snake case: Each word is separated by an underscore character:
+my_variable_name = "John"
+
+# tambien puedes asignar las variables de forma multiple
+x, y, z = "Orange", "Banana", "Cherry"
+print(x,y,z)
+
